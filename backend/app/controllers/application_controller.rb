35d5e7d62e7178bde45fe2b2744406f31457d5fb
sabakantaro <sbkryo290112@yahoo.co.jp>
2022-03-30 08:56:33 +0900

    frontend: マッチング機能を実装する #6

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
 
         skip_before_action :verify_authenticity_token
         helper_method :current_user, :user_signed_in?
+
+        # def current_user
+        #   @current_user ||= User.find(session[:user_id])
+        # end
+
+        # def user_signed_in?
+        #   current_user != nil
+        # end
       end
